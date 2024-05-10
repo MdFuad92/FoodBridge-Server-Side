53dbc0ce0be6788d65b248d3b9280634bb2a5885
Mdfuad92 <mdfuad98@gmail.com>
2024-05-10 23:55:35 +0600

    fixed an issue in a commit

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     const foodCollection = client.db('foodDB').collection('foods')
 
-    app.post('foods',async (req,res)=>{
+    app.post('/foods',async (req,res)=>{
         const food = req.body
         const result = await foodCollection.insertOne(food)
         res.send(result)
@@ -39,7 +39,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
