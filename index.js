20974c8b97364f987c9e7fcbe09411e733496450
Mdfuad92 <mdfuad98@gmail.com>
2024-05-12 02:45:51 +0600

    added id query

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -27,7 +27,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
+ 
     const foodCollection = client.db('foodDB').collection('foods')
 
     app.post('/foods',async (req,res)=>{
@@ -41,7 +41,15 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
     })
-    await client.db("admin").command({ ping: 1 });
+
+    app.get('/foods/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await foodCollection.findOne(query)
+      res.send(result)
+    })
+    //    Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
